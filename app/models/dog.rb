72da69d253ed5f40b444c7bd2bc352002cff6fc4
Justin Skord <jskord@gmail.com>
2018-03-17 11:30:47 -0500

    dobber survive conditionals

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -8,10 +8,18 @@ class Dog < ApplicationRecord
   end
 
   def enough_walks?
+    time_difference = Time.current - last_walk
+    minutes = (time_difference / 1.minute).round
+    hours = (time_difference / 1.hour).round
+    return hours
     return true
   end
 
   def enough_treats?
+    time_difference = Time.current - last_treat
+    minutes = (time_difference / 1.minute).round
+    hours = (time_difference / 1.hour).round
+    return hours
     return true
   end
 end

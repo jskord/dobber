507517fbb26c63cab294d0f4cb94607a7b0ea597
Justin Skord <jskord@gmail.com>
2018-03-16 22:38:21 -0500

    model work

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,3 +1,17 @@
 class Dog < ApplicationRecord
   has_many :users
+  def enough_food?
+    time_difference = Time.current - last_feeding
+    minutes = (time_difference / 1.minute).round
+    hours = (time_difference / 1.hour).round
+    return hours
+  end
+
+  def enough_walks?
+    return true
+  end
+
+  def enough_treats?
+    return true
+  end
 end

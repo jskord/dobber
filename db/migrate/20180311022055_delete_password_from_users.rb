0e92e4dd8a8b7caa702e5ed1c09494cf3dcd7652
Justin Skord <jskord@gmail.com>
2018-03-18 12:54:20 -0500

    migrations fixes

--- db/migrate/20180311022055_delete_password_from_users.rb
+++
@@ -1,5 +0,0 @@
-class DeletePasswordFromUsers < ActiveRecord::Migration[5.1]
-  def change
-    remove_column :users, :password, :string
-  end
-end
--- db/migrate/20180312033917_add_dog_id_to_user.rb
+++
@@ -1,5 +0,0 @@
-class AddDogIdToUser < ActiveRecord::Migration[5.1]
-  def change
-    add_column :users, :dog_id, :integer
-  end
-end

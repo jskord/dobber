f85e8755667cb4a1ae026d583aba51d76d423f49
Justin Skord <jskord@gmail.com>
2018-03-18 14:28:55 -0500

    commit

--- db/migrate/20180311213139_delete_user_column_in_user.rb
+++
@@ -1,5 +0,0 @@
-class DeleteUserColumnInUser < ActiveRecord::Migration[5.1]
-  def change
-    remove_column :dogs, :user, :string
-  end
-end
--- db/migrate/20180312044645_remoe.rb
+++
@@ -1,5 +0,0 @@
-class Remoe < ActiveRecord::Migration[5.1]
-  def change
-    remove_column :dogs, :user_id, :integer
-  end
-end

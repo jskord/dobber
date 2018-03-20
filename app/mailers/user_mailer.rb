d7292b20c564bc088c33a5e16d4a855dedf03ff9
Justin Skord <jskord@gmail.com>
2018-03-19 19:24:27 -0500

    did some email stuff

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,7 @@
 class UserMailer < ApplicationMailer
+  def welcome_email(user)
+    @user = user
+    @url  = 'https://vast-peak-96314.herokuapp.com/'
+    mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+  end
 end

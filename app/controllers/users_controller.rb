83ad8b64a8996c7fe335f6aef9a17814281d5e6c
Justin Skord <jskord@gmail.com>
2018-03-20 20:55:36 -0500

    signup email notification working in development

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,15 +4,21 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new(
+    @user = User.new(
       name: params[:name],
       email: params[:email],
       password: params[:password],
       password_confirmation: params[:password_confirmation],
       dog_id: 1
     )
-    if user.save
-      session[:user_id] = user.id
+    if @user.save
+      UserMailer.welcome_email(@user).deliver
+
+
+
+
+
+      session[:user_id] = @user.id
       flash[:success] = 'Successfully created account!'
       redirect_to '/'
     else
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'jskord@gmail.com'
+  default from: 'DobberTheDog1@gmail.com'
   layout 'mailer'
 end

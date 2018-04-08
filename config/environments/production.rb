6eaf9417bb966f948db1505c0f94a60fb4bca1b1
Justin Skord <jskord@gmail.com>
2018-04-08 15:40:59 -0500

    commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,8 +94,8 @@ Rails.application.configure do
 config.action_mailer.smtp_settings = {
  :address              => "smtp.gmail.com",
  :port                 => 587,
- :user_name            => ENV['gmail_username'],
- :password             => ENV['gmail_password'],
+ :user_name            => ENV['DobberTheDog1@gmail.com'],
+ :password             => ENV['xtremecardio'],
  :authentication       => "plain",
 :enable_starttls_auto => true
 }
